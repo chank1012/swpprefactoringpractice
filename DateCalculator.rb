46edc7de3210a5ea11cf1a771ac038bfb5bae2bf
prox <chank1012@naver.com>
2015-05-07 10:43:17 +0900

    date calculator, test have 3 fails

--- DateCalculator.rb
+++ DateCalculator.rb
@@ -1,8 +1,20 @@
 class DateCalculator
+  attr_accessor :days, :year
 
   def initialize(days)
     @days = days
     @year = 1980
+  end
+
+  def convert()
+    while (@days > 365) do
+      if leap_year?
+        add_leap_year
+      else
+        add_regular_year
+      end
+    end
+    return @year
   end
 
   def leap_year?
@@ -10,21 +22,19 @@ class DateCalculator
      (@year % 4 == 0 && @year % 100 != 0))
   end
 
-  def convert()
-    while (@days > 365) do
-      if leap_year?
-        if (@days > 366)
-          @days -= 366
-          @year += 1
-        end
-      else
-        @days -= 365
-        @year += 1
-      end
+  def add_leap_year
+    if (@days > 366)
+      @days -= 366
+      @year += 1
     end
-    return @year
   end
 
+  def add_regular_year
+    if (@days > 365)
+      @days -= 365
+      @year += 1
+    end
+  end
 end
 
 
